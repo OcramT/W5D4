f44ae8c4fcaccf08ae53015d9d46556024581a65
Marco Torre <marco.a.torre@gmail.com>
2021-01-28 18:09:17 -0500

    Adding URL project

---
+++ URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id        :bigint           not null, primary key
+#  long_url  :string           not null
+#  short_url :string           not null
+#  user_id   :integer          not null
+#
+class ShortenedUrl < ApplicationRecord
+    validates :long_url, presence: true
+    validates :long_url, uniqueness: true
+    validates :short_url, presence: true
+    validates :short_url, uniqueness: true
+    validates :user_id, presence: true
+    validates :user_id, uniqueness: true
+end
